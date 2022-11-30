eeb44c2364a294e57d7cee28ab9553199c9a0df3
tunacan <tunauzunokur@gmail.com>
2022-11-30 03:12:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,21 +12,27 @@ int main() {
 	// Returning the value of dot product of vector_1 and vector_2
 	cout << "dot product is : " << vector_1.dotP(vector_2)<< endl;
 
+
 	// Returning the angle between vector_1 and vector_2
 	cout << "angle between : " << vector_1.angleB(vector_2) << endl;
+
 
 	// Adding vector_1 and vector_2 up
 	cout << (vector_1.addV(vector_2)).x << ", " << (vector_1.addV(vector_2)).y << endl;
 
+
 	// Subtracting vector_1 and vector_2
 	cout << (vector_1.subtractV(vector_2)).x << ", " << (vector_1.subtractV(vector_2)).y << endl;
+
 
 	// Checking if vector_1 and vector_2 are equal, returns true or false
 	cout << vector_1.isEqual(vector_2) << endl;
 
+
 	// Returning perpendicular of a vector
 	vector2 vector_1_perp = vector_1.perpendicular();
 	cout << vector_1_perp.x << ", " << vector_1_perp.y << endl;
+
 
 	// Altering the x and y values of a desired vector
 	vector_1.set(6.0, 8.0);
